85abb336208a627aed2ca73e9fab450b2722d8dd
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-01-03 22:05:14 +0000

    Update links.jsx

--- src/components/navbar/links/links.jsx
+++ src/components/navbar/links/links.jsx
@@ -1,10 +1,32 @@
 import Link from "next/link";
 
-const Links = () => {
+export default function Links() {
   const links = [
     {
-        title: "Home",
-        path: ""
-    }
-];
-};
+      title: "Home",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+    {
+      title: "Gallery",
+      path: "/gallery",
+    },
+  ];
+
+  return (
+    <div>
+      {links.map((link) => (
+        <Link href={link.path} key={link.title}>
+          {link.title}
+        </Link>
+      ))}
+    </div>
+  );
+}
