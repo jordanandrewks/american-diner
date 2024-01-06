19440306f79b2d23669c3b61027141344562fc51
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-01-06 17:10:43 +0000

    save

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,29 @@
 import React from "react";
-import styles from "./footer.module.css"
+import styles from "./footer.module.css";
+import Image from "next/image";
 
 export default function Footer() {
-    return (
-        <div className={styles.footerContainer}>
-            Footer
-        </div>
-    );
+  return (
+    <div className={styles.footerContainer}>
+      <div className={styles.topSectionContainer}>Top</div>
+      <div className={styles.middleSectionContainer}>
+        <button className={styles.tiktokButton} >
+          <Image
+            src="/icons/footer/Tiktok-Black-Icon.png"
+            width={50}
+            height={50}
+            alt="tiktok"
+          />
+        </button>
+        <button>Instagram</button>
+        <button>Facebook</button>
+        <button>Twitter</button>
+      </div>
+      <div className={styles.bottomSectionContainer}>
+        <p className={styles.trademarkText}>
+          © American Diner, Inc. 2024. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
 }
