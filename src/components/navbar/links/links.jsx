92a9fba76b0b8fc847796b2240d31ad10fb622b7
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-01-30 11:53:19 +0000

    login and admin settings added

--- src/components/navbar/links/links.jsx
+++ src/components/navbar/links/links.jsx
@@ -2,25 +2,29 @@ import React from "react";
 import Link from "next/link";
 import styles from "../navbar.module.css";
 
+const links = [
+  {
+    title: "HOME",
+    path: "/",
+  },
+  {
+    title: "ABOUT",
+    path: "/about",
+  },
+  {
+    title: "CONTACT",
+    path: "/contact",
+  },
+  {
+    title: "GALLERY",
+    path: "/gallery",
+  },
+];
+
 export default function Links(className) {
-  const links = [
-    {
-      title: "HOME",
-      path: "/",
-    },
-    {
-      title: "ABOUT",
-      path: "/about",
-    },
-    {
-      title: "CONTACT",
-      path: "/contact",
-    },
-    {
-      title: "GALLERY",
-      path: "/gallery",
-    },
-  ];
+  // todo - add open, isOpen for hamburger menu ( if we need it... )
+  const session = false;
+  const isAdmin = false;
 
   return (
     <div className={styles.navbarLinks}>
@@ -29,6 +33,20 @@ export default function Links(className) {
           {link.title}
         </Link>
       ))}
+      {session ? (
+        <div className={styles.navbarUserContainer}>
+          {isAdmin && (
+            <Link href="/" key={"admin"} className={styles.navbarLink}>
+              ADMIN
+            </Link>
+          )}
+          <button>Logout</button>
+        </div>
+      ) : (
+        <div>Login</div>
+      )}
     </div>
   );
 }
+
+/* todo position the Admin and Login to the right side*/
