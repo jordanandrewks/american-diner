67194143d83f2fecd704f2b14ad3ec59b30b2430
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-05 19:52:02 +0000

    save

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,15 @@
-import { Head } from "next/head";
+import Head from "next/head";
 
 export default function Home() {
-  return;
-  <>
-    <Head>
-      <link
-        href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
-    <div>HomePage</div>;
-  </>;
+  return (
+    <>
+      <Head>
+        {/* <link
+          href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
+          rel="stylesheet"
+        /> */}
+      </Head>
+      <div>hello</div>
+    </>
+  );
 }
