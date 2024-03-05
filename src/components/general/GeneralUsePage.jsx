c1f795d5f938d2b9b661cc4e1abbe6cd42ea3139
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-05 14:53:54 +0000

    save

--- src/components/general/GeneralUsePage.jsx
+++ src/components/general/GeneralUsePage.jsx
@@ -4,10 +4,12 @@ import "/src/app/globals.css";
 
 export default function GeneralUsePage({ title, body }) {
   return (
-    <div className={styles.pageContainer}>
+    <div>
       <h1 className={styles.title}>{title}</h1>
-      {/* Below, needs to be change depending on the page. i.e. use this is as a proper component! */}
-      <div>{body}</div>
+      <div className={styles.pageContainer}>
+        {/* Below, needs to be change depending on the page. i.e. use this is as a proper component! */}
+        <div>{body}</div>
+      </div>
     </div>
   );
 }
