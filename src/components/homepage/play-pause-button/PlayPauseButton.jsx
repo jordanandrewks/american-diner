e622728305e3dcdb2b3e1a3c8f04d8a3f45ddad2
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-19 21:40:52 +0000

    icons added

--- src/components/homepage/play-pause-button/PlayPauseButton.jsx
+++ src/components/homepage/play-pause-button/PlayPauseButton.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import styles from "./PlayPauseButton.module.css";
+import Image from "next/image";
 
 const PlayPauseButton = ({ isPlaying, onButtonClickCallBack }) => {
-  const test = () => {
-    console.log(isPlaying);
-  };
+  const imageDetails = isPlaying
+    ? { path: "/icons/cookies/icons8-cookie-100.png", alt: "Pause" } // Assuming isPlaying means "Pause" icon
+    : { path: "/icons/cookies/icons8-play-100.png", alt: "Play" }; // Change to your actual "Play" icon path
 
   return (
     <div>
       <button
         onClick={() => {
           onButtonClickCallBack();
-          test();
         }}
         className={styles.container}
       >
-        Hello
+        <Image
+            src={imageDetails.path}
+            alt={imageDetails.alt}
+            width={30}
+            height={30}
+          />
+        I
       </button>
     </div>
   );
