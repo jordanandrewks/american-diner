b2a56f2ecbaf6e4520753dfaf8363e1c2bb6c590
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-19 21:48:48 +0000

    save

--- src/components/homepage/play-pause-button/PlayPauseButton.jsx
+++ src/components/homepage/play-pause-button/PlayPauseButton.jsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 const PlayPauseButton = ({ isPlaying, onButtonClickCallBack }) => {
   const imageDetails = isPlaying
-    ? { path: "/icons/video/icons8-pause-50.png", alt: "Pause" } // Assuming isPlaying means "Pause" icon
-    : { path: "/icons/video/icons8-play-50.png", alt: "Play" }; // Change to your actual "Play" icon path
+    ? { path: "/icons/video/icons8-play-50.png", alt: "play" }
+    : { path: "/icons/video/icons8-Pause-50.png", alt: "Pause" };
 
   return (
     <div>
