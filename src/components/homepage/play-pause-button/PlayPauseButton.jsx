1a817bce0fc12de06982a45487ea373647cbbf96
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-19 19:30:13 +0000

    save

--- src/components/homepage/play-pause-button/PlayPauseButton.jsx
+++ src/components/homepage/play-pause-button/PlayPauseButton.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import styles from "./PlayPauseButton.module.css";
 
 const PlayPauseButton = () => {
-    
+
   return (
     <div>
-      <button onClick={console.log("Hello")} className={styles.container}>Hello</button>
+      <button onClick={() => console.log("Hello")} className={styles.container}>Hello</button>
     </div>
   );
 };
