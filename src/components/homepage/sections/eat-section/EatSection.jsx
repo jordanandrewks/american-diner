71cbeca8dac92d3066e606e5b21b5e3060f9175f
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-20 09:53:28 +0000

    save

---
+++ src/components/homepage/sections/eat-section/EatSection.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EatSection = () => {
+  return <div>EatSection</div>;
+};
+
+export default EatSection;
---
+++ src/components/homepage/sections/live-sports-section/LiveSportsSection.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LiveSportsSection = () => {
+  return <div>LiveSportsSection</div>;
+};
+
+export default LiveSportsSection;
