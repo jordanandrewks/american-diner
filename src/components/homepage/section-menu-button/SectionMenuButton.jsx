eab2c531e7725c6c770c6b2e0dc5a7cf25b72e32
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-21 17:36:20 +0000

    live sports section added

--- src/components/homepage/section-menu-button/SectionMenuButton.jsx
+++ src/components/homepage/section-menu-button/SectionMenuButton.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./SectionMenuButton.module.css";
 
-const SectionMenuButton = ({ placeHolderTextLine1, placeHolderTextLine2 }) => {
+const SectionMenuButton = ({ placeHolderTextLine1, placeHolderTextLine2, placeHolderTextLine3 }) => {
   return (
     <div className={styles.container}>
       <div className={styles.buttonTextContainer}>
         <p className={styles.buttonText}>{placeHolderTextLine1}</p>
         <p className={styles.buttonText}>{placeHolderTextLine2}</p>
+        <p className={styles.buttonText}>{placeHolderTextLine3}</p>
       </div>
     </div>
   );
--- src/components/homepage/sections/live-sports-section/LiveSportsSection.jsx
+++ src/components/homepage/sections/live-sports-section/LiveSportsSection.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import SectionContainer from "../SectionContainer";
+import styles from "./LiveSportsSection.module.css";
+import SectionMenuButton from "../../section-menu-button/SectionMenuButton";
 
 const LiveSportsSection = () => {
-  return <div>LiveSportsSection</div>;
+  return (
+    <SectionContainer>
+      <div className={styles.container}>
+        <div className={styles.eventContainer}> Left </div>
+        <SectionMenuButton
+          placeHolderTextLine1={"VIEW"}
+          placeHolderTextLine2={"LIVE"}
+          placeHolderTextLine3={"SPORTS"}
+        />
+      </div>
+    </SectionContainer>
+  );
 };
 
 export default LiveSportsSection;
