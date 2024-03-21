e95fc4410e9b6a597a75a8836253e3ca51e0d0dc
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-21 08:47:38 +0000

    save

--- src/components/homepage/sections/eat-section/EatSection.jsx
+++ src/components/homepage/sections/eat-section/EatSection.jsx
@@ -9,23 +9,30 @@ const EatSection = () => {
         <p className={styles.subTitle}>2023 </p>
       </div> */}
 
-      {/* Top Section, Winners of 2023 awards*/}
-      <div className={styles.awardContainer}>
-        <div className={styles.leftSection}>Left</div>
-        <div className={styles.rightSection}>Right</div>
-      </div>
-
       <div className={styles.container}>
-        <div className={styles.item}>item 1</div>
-        <div className={styles.item}>item 2</div>
-        <div className={styles.item}>item 3</div>
-        <div className={styles.item}>item 4</div>
+        <div className={styles.leftContainer}>
+          {/* Top Section, Winners of 2023 awards*/}
+          <div className={styles.awardContainer}>
+            <div className={styles.leftSection}>Left</div>
+            <div className={styles.rightSection}>Right</div>
+          </div>
+
+          <div className={styles.itemContainer}>
+            <div className={styles.item}>item 1</div>
+            <div className={styles.item}>item 2</div>
+            <div className={styles.item}>item 3</div>
+            <div className={styles.item}>item 4</div>
+          </div>
+        </div>
+        <div className={styles.rightContainer}>
+          <button>F</button>
+        </div>
       </div>
     </SectionContainer>
 
     // Todo - Make the items a component...
     // inside this component - Show Image, Title
-    
+
     /* Todo - Add a button on the right of this container,
      what will fast forward you to the foods section. 
      We can do the same for the Drinks and anything else...
