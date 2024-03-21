a5f8e56d2f9ba6047b3bbd01384f3a389adf8834
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-21 17:38:54 +0000

    live events section added

--- src/components/homepage/sections/vibes-section/VibesSection.jsx
+++ src/components/homepage/sections/vibes-section/VibesSection.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import SectionContainer from "../SectionContainer";
+import styles from "./VibesSection.module.css";
+import SectionMenuButton from "../../section-menu-button/SectionMenuButton";
 
 const Vibes = () => {
-  return <div>Vibes</div>;
+  return (
+    <SectionContainer>
+      <div className={styles.container}>
+        <div className={styles.eventContainer}> Left </div>
+        <SectionMenuButton
+          placeHolderTextLine1={"VIEW"}
+          placeHolderTextLine2={"UPCOMING"}
+          placeHolderTextLine3={"EVENTS"}
+        />
+      </div>
+    </SectionContainer>
+  );
 };
 
 export default Vibes;
