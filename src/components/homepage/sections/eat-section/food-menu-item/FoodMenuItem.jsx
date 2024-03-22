8e0cc9ea418d3de9cfbba9c31a4461392993a2ad
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-22 14:17:41 +0000

    save
    Image resize, component Image code changed

--- src/components/homepage/sections/eat-section/food-menu-item/FoodMenuItem.jsx
+++ src/components/homepage/sections/eat-section/food-menu-item/FoodMenuItem.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import styles from "./FoodMenuItem.module.css";
+import Image from "next/image";
 
 const MenuItem = ({ demoName }) => {
-  return <div className={styles.container}>{demoName}</div>;
+  return (
+    <div className={styles.container}>
+      {/* <div className={styles.imageContainer}> */}
+      <Image
+        className={styles.imageContainer}
+        src={"/homepage/food-highlights/PizzaAi-Highlight.png"}
+        // src={"/homepage/food-highlights/AmericanDiner-Burger-Highlight.png"}
+        // width={500}
+        // height={600}
+        fill={true}
+        alt={"Burger"}
+      />
+      {/* <Image src={image.url} height={100} width={100} alt={image.alt} /> */}
+    </div>
+    // </div>
+  );
 };
 
 export default MenuItem;
