c659e8cf205ffe5da6c797144a0c950ba865a584
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-22 17:38:52 +0000

    more pics added, highlight containe in progress

---
+++ src/components/homepage/sections/eat-section/highlights-container/HighlightsContainer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./HighlightsContainer.module.css";
+
+const HighlightsContainer = () => {
+  return (
+    <div>HighlightsContainer</div>
+  )
+}
+
+export default HighlightsContainer;
---
+++ src/components/homepage/sections/eat-section/highlights-container/highlight-item/HighlightItem.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./HighlightItem.module.css";
+
+const HighlightItem = () => {
+  return (
+    <div>HighlightItem</div>
+  )
+}
+
+export default HighlightItem;
