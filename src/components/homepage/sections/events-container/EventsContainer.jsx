84cbe079972e391d1842cdc449858fbff4204df4
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-22 23:36:01 +0000

    events added

---
+++ src/components/homepage/sections/events-container/EventsContainer.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import styles from "./EventsContainer.module.css";
+import Image from "next/image";
+
+const EventsContainer = ({ eventLinks }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Start by fading out
+      setIsVisible(false);
+
+      // Change the event after the fade out transition is done
+      setTimeout(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % eventLinks.length);
+
+        // Fade back in
+        setIsVisible(true);
+      }, 1000); // This duration should match the CSS transition duration
+    }, 4000); // Includes 1s fade out, 1s fade in, and 2s display time
+
+    return () => clearInterval(interval);
+  }, [eventLinks.length]);
+
+  const currentEvent = eventLinks[currentIndex];
+
+  return (
+    <div className={styles.container}>
+      <Image
+        src={currentEvent.src}
+        alt={currentEvent.alt}
+        layout="fill"
+        objectFit="cover"
+        className={styles.imageContainer}
+      ></Image>
+      <div className={styles.overlay}></div>
+      <div className={styles.textContainer}>
+        <p className={styles.eventTitle}>{currentEvent.eventTitle}</p>
+        <div className={styles.subTitleContainer}>
+          <p>{currentEvent.subTitle1}</p>
+          <p>{currentEvent.subTitle2}</p>
+        </div>
+        <p className={styles.eventDate}>{currentEvent.eventDate}</p>
+      </div>
+    </div>
+  );
+};
+
+export default EventsContainer;
--- src/components/homepage/sections/vibes-section/VibesSection.jsx
+++ src/components/homepage/sections/vibes-section/VibesSection.jsx
@@ -2,12 +2,34 @@ import React from "react";
 import SectionContainer from "../SectionContainer";
 import styles from "./VibesSection.module.css";
 import SectionMenuButton from "../../section-menu-button/SectionMenuButton";
+import EventsContainer from "../events-container/EventsContainer";
 
 const Vibes = () => {
+  const eventLinks = [
+    {
+      src: "/homepage/events-highlights/events/pexels-pixabay-274192.webp",
+      alt: "House Night",
+      url: "/",
+      eventTitle: "HOUSE NIGHT : 80S THROWBACK",
+      subTitle1: "PRESENTED BY SOUL SELECTION",
+      subTitle2: null,
+      eventDate: "FRIDAY 3RD MAY",
+    },
+    {
+      src: "/homepage/events-highlights/sports/pexels-gotta-be-worth-it-5215129.webp",
+      alt: "Beer Tasting Event",
+      url: "/",
+      eventTitle: "BEER SAMPLERS",
+      subTitle1: "PRESENTED BY LONDON DINING CLUB",
+      subTitle2: null,
+      eventDate: "WEDNESDAY 4TH APRIL",
+    },
+  ];
+
   return (
     <SectionContainer>
       <div className={styles.container}>
-        <div className={styles.eventContainer}> Left </div>
+        <EventsContainer eventLinks={eventLinks}/>
         <SectionMenuButton
           placeHolderTextLine1={"VIEW"}
           placeHolderTextLine2={"UPCOMING"}
