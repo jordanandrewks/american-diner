716eddca1dcca4c3f7ce1a6e101e9d088f9a6a35
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-22 18:55:25 +0000

    save

--- src/components/homepage/sections/eat-section/highlights-container/highlight-item/HighlightItem.jsx
+++ src/components/homepage/sections/eat-section/highlights-container/highlight-item/HighlightItem.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./HighlightItem.module.css";
+import Image from "next/image";
 
 const HighlightItem = ({
   src,
   alt,
-  url,
+  url, // TODO Get this to work...
   title1,
   title2,
   subTitle1,
@@ -13,8 +14,29 @@ const HighlightItem = ({
   buttonTitleLine2,
   buttonTitleLine3,
 }) => {
-  console.log(title1);
-  return <div className={styles.container}>{title2}</div>;
+  console.log(title2);
+  return (
+    <div className={styles.container}>
+      <Image src={src} alt={alt} layout="fill" objectFit="cover" />
+      <div className={styles.textContainer}>
+        <div className={styles.titleContainer}>
+          <p>{title1}</p>
+          <p>{title2}</p>
+        </div>
+        <div className={styles.subTitleContainer}>
+          <p>{subTitle1}</p>
+          <p>{subTitle2}</p>
+        </div>
+      </div>
+      <div className={styles.buttonContainer}>
+        <div className={styles.buttonTextContainer}>
+          <p>{buttonTitleLine1}</p>
+          <p>{buttonTitleLine2}</p>
+          <p>{buttonTitleLine3}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HighlightItem;
