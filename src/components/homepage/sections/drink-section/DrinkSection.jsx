fc07f5e738fe5284cd8711d39f80c4c4d4abd5f9
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-22 21:58:00 +0000

    drink containers added

--- src/components/homepage/sections/drink-section/DrinkSection.jsx
+++ src/components/homepage/sections/drink-section/DrinkSection.jsx
@@ -5,14 +5,46 @@ import DrinkMenuItem from "./drink-menu-item/DrinkMenuItem";
 import SectionMenuButton from "../../section-menu-button/SectionMenuButton";
 
 const DrinkSection = () => {
+  const drinkLinks = [
+    {
+      src: "/homepage/food-highlights/FriesAI-Highlight.png",
+      alt: "Beers",
+      url: "/",
+      displayText: "BEERS",
+    },
+    {
+      src: "/homepage/food-highlights/AmericanDiner-Burger-Highlight.png",
+      alt: "Wines",
+      url: "/",
+      displayText: "WINES",
+    },
+    {
+      src: "/homepage/food-highlights/BBQ Ribs-Highlight.png",
+      alt: "Whiskies",
+      url: "/",
+      displayText: "WHISKIES",
+    },
+    {
+      src: "/homepage/food-highlights/PizzaAi-Highlight.png",
+      alt: "Soda",
+      url: "/",
+      displayText: "SODAS",
+    },
+  ];
+
   return (
     <SectionContainer>
       <div className={styles.container}>
         <div className={styles.drinksContainer}>
-          <DrinkMenuItem demoPlaceHolderText={"Item 1"} />
-          <DrinkMenuItem demoPlaceHolderText={"Item 2"} />
-          <DrinkMenuItem demoPlaceHolderText={"Item 3"} />
-          <DrinkMenuItem demoPlaceHolderText={"Item 4"} />
+          {drinkLinks.map((item) => (
+            <DrinkMenuItem
+              key={item.alt}
+              imageSrc={item.src}
+              imageAlt={item.alt}
+              imageLink={item.url}
+              bannerText={item.displayText}
+            />
+          ))}
         </div>
         <SectionMenuButton
           placeHolderTextLine1={"DRINKS"}
--- src/components/homepage/sections/drink-section/drink-menu-item/DrinkMenuItem.jsx
+++ src/components/homepage/sections/drink-section/drink-menu-item/DrinkMenuItem.jsx
@@ -1,8 +1,35 @@
-import React from "react";
-import styles from "./DrinkMenuItem.module.css";
+"use client";
 
-const DrinkMenuItem = ({ demoPlaceHolderText }) => {
-  return <div className={styles.container}>{demoPlaceHolderText}</div>;
+import React, { useState, useRef, useEffect } from "react";
+import styles from "./DrinkMenuItem.module.css";
+import Image from "next/image";
+
+const DrinkMenuItem = ({ imageSrc, imageAlt, imageLink, bannerText }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    if (imageRef.current) {
+      const imageStyle = imageRef.current.style;
+      imageStyle.transition = "transform .15s ease-in-out";
+      imageStyle.transform = isHovered ? "scale(1.1)" : "scale(1)";
+    }
+  }, [isHovered]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer} ref={imageRef}>
+        <Image src={imageSrc} alt={imageAlt} layout="fill" objectFit="cover" />
+      </div>
+      <div
+        className={styles.banner}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <p>{bannerText}</p>
+      </div>
+    </div>
+  );
 };
 
 export default DrinkMenuItem;
