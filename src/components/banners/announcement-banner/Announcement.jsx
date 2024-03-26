c8cb84a8e74c607d892c3b64d81ba22679e8a9a5
Jordan S. K <26313566+jordanandrewks@users.noreply.github.com>
2024-03-26 13:12:21 +0000

    fade in banner

--- src/components/banners/announcement-banner/Announcement.jsx
+++ src/components/banners/announcement-banner/Announcement.jsx
@@ -1,21 +1,30 @@
-"use client";
+"use client"
 
+import React, { useEffect, useState } from 'react'; // Import useEffect and useState
 import styles from "./Announcement.module.css";
 import useFetchBannerAnnouncement from "@/hooks/useFetchBannerAnnouncement";
 
 const AnnouncementBanner = () => {
-  const { announcement, banner, isLoading, error } =
-    useFetchBannerAnnouncement();
+  const { announcement, banner, isLoading, error } = useFetchBannerAnnouncement();
+  const [showBanner, setShowBanner] = useState(false);
+
+  useEffect(() => {
+    if (banner) {
+      setShowBanner(true); // Show the banner if `banner` is truthy
+    }
+  }, [banner]); // This effect runs whenever `banner` changes
 
   console.log(announcement, banner, isLoading, error);
 
-  return banner ? (
-    <div className={styles.container}>
+  const containerClass = showBanner ? `${styles.container} ${styles.show}` : styles.container;
+
+  return (
+    <div className={containerClass}>
       <div className={styles.scrollContainer}>
         <div className={styles.scrollText}>{announcement}</div>
       </div>
     </div>
-  ) : null;
+  );
 };
 
 export default AnnouncementBanner;
